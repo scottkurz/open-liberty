d824662517a868a74a05e702732e7e777ed71ac3
Scott Kurz <skurz@us.ibm.com>
2022-02-11 18:27:24 -0500

    Remove isNullable() deprecated method
    Signed-off-by: Scott Kurz <skurz@us.ibm.com>

--- dev/com.ibm.ws.jbatch.cdi/src/com/ibm/ws/jbatch/cdi/JobOpProducerBean.java
+++ dev/com.ibm.ws.jbatch.cdi/src/com/ibm/ws/jbatch/cdi/JobOpProducerBean.java
@@ -106,9 +106,4 @@ public class JobOpProducerBean implements Bean<JobOperator> {
         public boolean isAlternative() {
             return false;
         }
-
-        @Override
-        public boolean isNullable() {
-            return false;
-        }
 }
